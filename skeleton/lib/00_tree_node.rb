4d1a9534735061f87fb35bb1e57c52ebf1e53b22
poorzhu <thezhukeeper@gmail.com>
2019-05-17 14:35:01 -0700

    Create node class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -9,8 +9,19 @@ class PolyTreeNode
   end
 
   def parent=(node)
+    @parent.children.delete_at(@parent.children.find_index(self)) unless @parent.nil?
     @parent = node
-    node.children += [self]
+    node.children << self unless node.nil? || node.children.include?(self)
+  end
+
+  def add_child(node)
+    node.parent = self
+    @children << node unless @children.include?(node)
+  end
+
+  def remove_child(node)
+    node.parent = nil
+    raise "Node is not a child." unless @children.include?(node)
   end
 
 end
